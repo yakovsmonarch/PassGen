ec7c8984461dd0abdd37684e5ef4a0e1487d49b2
yakovsmonarch <frag.2010@yandex.ru>
2025-09-15 13:15:04 +0800

    Добавил проект юнит тестирования.

---
+++ TestPassGen/GeneratePasswordTest.cs
@@ -0,0 +1,15 @@
+﻿namespace TestPassGen;
+
+public class GeneratePasswordTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
